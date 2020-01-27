a8115224cab511d1686cabcef53f0e785ac07b23
BarriF13 <info@bdesignstudio.co.uk>
2020-01-27 16:09:28 +0000

    Add top_up method

--- lib/oyster_card.rb
+++ lib/oyster_card.rb
@@ -1,1 +1,11 @@
 
+# write a class to solve the first error
+class OysterCard
+  attr_reader :balance
+  def initialize()
+    @balance = 0
+  end
+
+  def top_up(credit)
+  end
+end
--- spec/oystercar_spec.rb
+++ spec/oystercar_spec.rb
@@ -1,4 +1,17 @@
 require 'oyster_card'
 describe OysterCard do
-  
+#--First error--uninitialized constant error--NameError:uninitialized constant OysterCard--./spec/oystercar_spec.rb:2:in `<top (required)>'
+#------first story
+# In order to use public transport
+# As a customer
+# I want money on my card
+it 'I want money on my card, my card balance is zero' do
+  expect(subject.balance).to eq(0)
+end
+# In order to keep using public transport
+# As a customer
+# I want to add money to my card
+it 'I need to top up' do
+  expect(subject).to respond_to(:top_up).with(1).argument
+end
 end
