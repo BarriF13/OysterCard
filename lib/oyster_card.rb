549f1b32f866b893e9d4eb7178f200d7a4099c1d
BarriF13 <info@bdesignstudio.co.uk>
2020-01-30 18:38:54 +0000

    Forth attempt to learn

--- lib/oyster_card.rb
+++ lib/oyster_card.rb
@@ -1,46 +1,15 @@
-
-# write a class to solve the first error
-
-class OysterCard
-  MAX_TOP = 90
-  MIN_BALANCE = 10
-  MIN_CHARGE = 1
-
-  attr_reader :balance ,:card_in_journey, :fare, :entry_station, :exit_station, :saved_journeys
-  def initialize()
-    @balance = 15
-    @fare = 0
-    @card_in_journey = false
-    @entry_station = nil
-    @saved_journeys = []
-    @exit_station = nil
+class Oystercard
+  MAXIMUM_BALANCE = 90
+attr_reader :balance
+  # def balance
+  #   0
+  # end
+  def initialize
+    @balance = 0
   end
 
-  def in_journey?
-    !!entry_station
-  end
-
-  def top_up(credit)
-
-    raise "Maximum balance of #{MAX_TOP} exceeded" if( @balance + credit) > MAX_TOP
-    @balance += credit
-  end
-
-  def touch_in(station)
-    fail "required #{MIN_BALANCE} for your journey" if @balance < MIN_BALANCE
-    @card_in_journey = true
-    @entry_station = station
-    @saved_journeys.push(@entry_station)
-  end
-
-  def touch_out(station)
-    @card_in_journey = false
-    deduct(MIN_CHARGE)
-    @exit_station = station
-    @saved_journeys.push(@exit_station)
-  end
-
-  def deduct(fare)
-    @balance -= fare
+  def top_up(amount)
+    raise 'Maximum balance  of #{maximum_balance} exceeded' if @balance + amount > MAXIMUM_BALANCE
+    @balance = @balance + amount
   end
 end
